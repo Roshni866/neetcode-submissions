07a540c19054d724010d7039193bcb730b74fd4c
Roshni Bagga <roshnibagga8606@gmail.com>
2026-06-06 10:50:16 +0530

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>> res;
+        for (string s :strs){
+            vector<int> count(26,0);
+            for(char c:s){
+                count[c-'a']++;
+            }
+            string key=" ";
+            for(int i=0;i<26;i++){
+                key+='#' + count[i];
+            }
+            res[key].push_back(s);
+        }
+        vector<vector<string>> ans;
+        for(auto it :res){
+            ans.push_back(it.second);
+        }
+        return ans;
+        
+    }
+};
